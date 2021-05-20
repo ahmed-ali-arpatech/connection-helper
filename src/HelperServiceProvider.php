50c7b36f9320c54247a882a8cce8cbbaa5ba59b6
Ahmed Ali <ahmed.ali@arpatech.com>
2021-05-20 17:34:32 +0500

    initial commit

---
+++ src/HelperServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Connection\Helper;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelperServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // $this->app->make('Ccm\Helpers\Greeter');        
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {  
+
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+
+        if (file_exists($file = app_path('Helpers/helpers.php')))
+        {
+            require $file;
+        }
+    }
+}
